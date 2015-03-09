e708a5d6e34a8145eee0c9dfefda8769c2dedd9a
Jean Frg <jeanfrg@gmail.com>
2015-03-09 06:57:39 +0100

    fixed form submit bug

--- sauce/js/script.js
+++ sauce/js/script.js
@@ -12,29 +12,6 @@
         factory(jQuery);
     }
 }(function($) {
-    // defaults may be overridden
-    $.fn.ftakar.defaults = {
-            savedDataName: 'FTAKAR',
-            // saveOnInterval: 200,
-            saveOnInterval: 300,
-            saveOnChange: false,
-            clearOnSubmit: true,
-            expireInMs: false,
-            // priority by order
-            idAttribs: ['id', 'data-ftakar'], 
-            beforeSave: function(element){ console.info('FTAKAR: data is being saved', element) },
-            // $(document).trigger( "ftakar__beforeSave" );
-            onSave: function(element){ console.info('FTAKAR: data saved', element) },
-            // $(document).trigger( "ftakar__onSave" );
-            beforeLoad: function(){ console.info('FTAKAR: data is being loaded') },
-            // $(document).trigger( "ftakar__beforeLoad" );
-            onLoad: function(){ console.info('FTAKAR: data has loaded') },
-            // $(document).trigger( "ftakar__onLoad" );
-            beforeClear: function(){ console.info('FTAKAR: data is going to be cleared') },
-            // $(document).trigger( "ftakar__beforeClear" );
-            onClear: function(){ console.info('FTAKAR: data cleared') }
-            // $(document).trigger( "ftakar__onClear" );
-        };
     $.fn.ftakar = function(options) {
         // default settings
         var settings = $.extend($.fn.ftakar.defaults, options),
@@ -49,7 +26,8 @@
         };
 
         if (supports_html5_storage()) {
-            var store = {
+            var $this = $(this),
+            store = {
                 get: function() {
                     var a = JSON.parse(localStorage.getItem(settings.savedDataName));
                     return (typeof(a) === 'object' && a != null) ? a : {};
@@ -85,66 +63,80 @@
                     return a;
                 }
             },
-            save = function(element) {
-                if (element) {
-                    // get data
-                    var data = store.get();
-                    
-                    var elementId = key.create(element);
-                    if (elementId) {
-                        settings.beforeSave.call(element);
+            elementData = {
+                save: function(element) {
+                    if (element) {
+                        // get data
+                        var data = store.get();
+                        
+                        var elementId = key.create(element);
+                        if (elementId) {
+                            settings.beforeSave.call();
 
-                        var now = new Date().getMilliseconds();
-                        var expires = (settings.expiresInMs) ? parseInt(now.toString()) + settings.expiresInMs : null;
+                            var now = new Date().getMilliseconds();
+                            var expires = (settings.expiresInMs) ? parseInt(now.toString()) + settings.expiresInMs : null;
 
-                        data[elementId] = { 
-                                            expires: expires,
-                                            val: $(element).val()
-                                        };
+                            data[elementId] = { 
+                                                expires: expires,
+                                                val: $(element).val()
+                                            };
 
-                        // save data
-                        store.set(data);
-                        settings.onSave.call(element);
-                        return true;
+                            // save data
+                            store.set(data);
+                            settings.onSave.call();
+                            return true;
+                        }
                     }
-                }
 
-                return false;
-            },
-            /*delete = function(element) {
-                if (element) {
-                    // get data
-                    var data = data.get();
-                    
-                    var elementId = key.create(element);
-                    if (elementId) {
-                        delete data[elementId];
+                    return false;
+                },
+                destroy: function(element) {
+                    if (element) {
+                        // get data
+                        var data = store.get();
+                        
+                        var elementId = key.create(element);
+                        if (elementId) {
+                            settings.beforeDelete.call();
 
-                        // save data
-                        data.set(data);
-                        return true;
+                            delete data[elementId];
+
+                            settings.onDelete.call();
+                            // save data
+                            store.set(data);
+                            return true;
+                        }
                     }
-                }
 
-                return false;
-            },*/
-            hasExpired = function(data) {
-                // check if element has expired
-                var now = new Date().getMilliseconds();;
-                return (data.expires) ? (now > data.expires) : false;
+                    return false;
+                },
+                hasExpired: function(data) {
+                    // check if element has expired
+                    var now = new Date().getMilliseconds();;
+                    return (data.expires) ? (now > data.expires) : false;
+                }
             };
 
             if (settings.clearOnSubmit) {
-                this.closest('form').submit(function() {
-                    // clear data
-                    store.destroy();
+                // Most of this can be deleted when form domains are implemented
+                /*var input = $('input[type=submit]');
+                var form = input.length > 0 ? $(input[0].form) : $();*/
+
+                this.closest('form')
+                    .submit(function(e){
+                        e.preventDefault();
+                        // when closest form is submitted
+                        $this.each(function(index, element){
+                            // delete element data
+                            elementData.destroy(this);
+                        });
                 });
             }
 
             if (settings.saveOnInterval && !isNaN(settings.saveOnInterval) && parseInt(settings.saveOnInterval) > 0) {
                 setInterval(function(){ 
                     // save input
-                    save(this);
+                    elementData.save(this);
                 }, parseInt(settings.saveOnInterval));
             }
 
@@ -155,7 +147,7 @@
                 for (var k in data) {
                     // loop through saved data
                     // check if data has expired
-                    if (!hasExpired(data[k])) {
+                    if (!elementData.hasExpired(data[k])) {
                         // get key
                         var id = key.get(k);
                         // find respective element & set saved data
@@ -174,11 +166,40 @@
 
             if (settings.saveOnChange) {
                 this.change(function(){
-                    save(this);
+                    elementData.save(this);
                 });
             } 
         }
 
         return this;
     }
+    
+    // defaults may be overridden
+    $.fn.ftakar.defaults = {
+        savedDataName: 'FTAKAR',
+        // saveOnInterval: false,
+        // saveOnInterval: 2000,
+        saveOnInterval: false, // TODO
+        saveOnChange: true,
+        clearOnSubmit: true,
+        expireInMs: false, // TODO
+        // priority by order of attribute
+        idAttribs: ['id', 'data-ftakar'], 
+        beforeSave: function(){ console.info('FTAKAR: data is being saved', $(this)) },
+        // $(document).trigger( "ftakar__beforeSave" ); // TODO
+        onSave: function(){ console.info('FTAKAR: data saved', $(this)) },
+        // $(document).trigger( "ftakar__onSave" ); // TODO
+        beforeLoad: function(){ console.info('FTAKAR: data is being loaded') },
+        // $(document).trigger( "ftakar__beforeLoad" ); // TODO
+        onLoad: function(){ console.info('FTAKAR: data has loaded') },
+        // $(document).trigger( "ftakar__onLoad" ); // TODO
+        beforeDelete: function(){ console.info('FTAKAR: element data is going to be deleted', $(this)) },
+        // $(document).trigger( "ftakar__beforeDelete" ); // TODO
+        onDelete: function(){ console.info('FTAKAR: element data deleted', $(this)) },
+        // $(document).trigger( "ftakar__onDelete" ); // TODO
+        beforeClear: function(){ console.info('FTAKAR: data is going to be cleared') },
+        // $(document).trigger( "ftakar__beforeClear" ); // TODO
+        onClear: function(){ console.info('FTAKAR: data cleared') }
+        // $(document).trigger( "ftakar__onClear" ); // TODO
+    };
 }));
