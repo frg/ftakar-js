af7d3ccc7a707631ef506b494b151cd3b29e9aa3
Jean Frg <jeanfrg@gmail.com>
2015-04-24 12:21:34 +0200

    first major version release

--- README.md
+++ README.md
@@ -1,30 +1,27 @@
 # Ftakar [![Code Climate](https://codeclimate.com/github/frg/ftakar-js/badges/gpa.svg)](https://codeclimate.com/github/frg/ftakar-js)
-### A jQuery plugin that saves input data using Local Storage
+### A jQuery plugin that saves element data using Local Storage
 
-## Version
-### **THIS PLUGIN IS STILL IN DEVELOPMENT
-
-##
 Ftakar is a small jQuery plugin that I decided to implement as extra functionality to my projects. Even though the plugin idea is simple it adds a considerable amount to the traditional html forms.
 
-The plugin is in its very early stages of implementation therefore here's my ToDo list up front.
+## Version
+1.0
 
 ## Todo
  - **TEST**
- - Fix interval save
- - ~~Fix data expires~~
+ - ~~Implement interval save~~
+ - ~~Implement data expires~~
  - ~~Implement checkbox save~~
  - ~~Implement radio button save~~
  - ~~Implement select save~~
  - ~~Save forms within uri namespace~~ (can be implemented using the uri in the "savedDataName" setting)
 
 ##  Installation
-Until a major version is committed script file is within 'examples/js/ftakar.js'
+Import jQuery 1.11.2 or greater
+Import "sauce/ftakar-1.0.min.js"
 
 ##  Usage
-1. Import jQuery
-2. ~~Import ftakar-js.js from the sauce folder~~
-2. *For the moment please import as instructed in **Installation*
+1. Import jQuery 1.11.2 or greater
+2. Import "sauce/ftakar-1.0.min.js"
 3. '$('input').ftakar();'
 
 Example:
@@ -37,6 +34,14 @@ Example:
     <input type="checkbox" name="vehicle" value="Car" id="sivcy">I have a car
     <input type="date" name="bday" id="mdqgm"
     <input type="color" name="favcolor" id="ljwon">
+    <select id="khupoa">
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+      <option value="mercedes">Mercedes</option>
+      <option value="audi">Audi</option>
+    </select>
+
+    <input type="submit" value="Submit">
 </form>
 ```
 
@@ -57,6 +62,7 @@ Possible options:
 * `clearOnSubmit` – when the closest parent form is submitted the element data will be deleted – *boolean*: `true`
 * `idAttribs` - when defined the plugin will search for the the first valid id before going onto the next - *array*: `['id', 'name', 'data-ftakar']`
 * `expireInMs` - ms to save data for element - *int* / *boolean*: `false`
+* `saveOnInterval` – save every n ms – *int* / *boolean* : `false`
 
 * `beforeSave` – function called before save of every element – *function*: `function(){}`
 * `onSave` – function called after save of every element – *function*: `function(){}`
--- examples/js/ftakar.js
+++ examples/js/ftakar.js
@@ -12,221 +12,234 @@
         factory(jQuery);
     }
 }(function($) {
-    $.fn.ftakar = function(options) {
-        // default settings
-        var settings = $.extend($.fn.ftakar.defaults, options),
-        supports_html5_storage = function () {
-            // check if browser supports local storage
-            try {
-                return 'localStorage' in window && window['localStorage'] !== null;
-            } catch (e) {
-                // console.info('FTAKAR: html5 storage not supported.');
+  // https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setInterval#The_.22this.22_problem
+  var __nativeSI__ = window.setInterval;
+  var setInterval = function (vCallback, nDelay /*, argumentToPass1, argumentToPass2, etc. */) {
+    var oThis = this, aArgs = Array.prototype.slice.call(arguments, 2);
+    return __nativeSI__(vCallback instanceof Function ? function () {
+      vCallback.apply(oThis, aArgs);
+    } : vCallback, nDelay);
+  };
+
+  $.fn.ftakar = function(options) {
+    // default settings
+    var settings = $.extend($.fn.ftakar.defaults, options),
+    supports_html5_storage = function () {
+        // check if browser supports local storage
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch (e) {
+            // console.info('FTAKAR: html5 storage not supported.');
+            return false;
+        }
+    };
+
+    // check for loacl storage support
+    if (supports_html5_storage()) {
+        var $this = $(this),
+        store = {
+            get: function() {
+                var a = JSON.parse(localStorage.getItem(settings.savedDataName));
+                return (typeof(a) === 'object' && a != null) ? a : {};
+            },
+            set: function(data) {
+                localStorage.setItem(settings.savedDataName, JSON.stringify(data));
+            },
+            destroy: function() {
+                localStorage.removeItem(settings.savedDataName);
+            }
+        },
+        key = {
+            // this key generations stuff can and should be improved
+            create: function(element) {
+                for (var i = 0; i < settings.idAttribs.length; i++) {
+                    // check if attribute has value
+                    if ($(element).attr(settings.idAttribs[i]) != null) {
+                        // escape strings
+                        return escape(settings.idAttribs[i]) + '~' + escape($(element).attr(settings.idAttribs[i]));
+                    }
+                }
+
                 return false;
+            },
+            get: function(key) {
+                // split
+                var a = key.split(/~(.+)?/);
+                // unescape
+                for (var i = 0; i < a.length; i++) {
+                    a[i] = unescape(a[i]);
+                }
+                return a;
+            }
+        },
+        elementData = {
+            save: function(element) {
+                if (element) {
+                    // get data
+                    var data = store.get();
+
+                    var elementId = key.create(element);
+                    if (elementId) {
+                        settings.beforeSave.call();
+
+                        var now = new Date().getTime();
+                        var expires = (settings.expireInMs) ? parseInt(now.toString()) + settings.expireInMs : null;
+
+                        var toSave = "";
+                        switch ($(element).attr('type')){
+                          case 'radio':
+                          case 'checkbox':
+                            toSave = $(element).prop('checked');
+                            break;
+                          case 'select':
+                          case 'input':
+                          default:
+                            toSave = $(element).val();
+                        }
+
+                        data[elementId] = {
+                                            expires: expires,
+                                            val: toSave
+                                        };
+
+                        // save data
+                        store.set(data);
+                        settings.onSave.call();
+                        return true;
+                    }
+                }
+
+                return false;
+            },
+            destroy: function(element) {
+                if (element) {
+                    // get data
+                    var data = store.get();
+
+                    var elementId = key.create(element);
+                    if (elementId) {
+                        settings.beforeDelete.call();
+
+                        delete data[elementId];
+
+                        settings.onDelete.call();
+                        // save data
+                        store.set(data);
+                        return true;
+                    }
+                }
+
+                return false;
+            },
+            hasExpired: function(data) {
+                // check if element has expired
+                var now = new Date().getTime();
+                return (data.expires) ? (now > data.expires) : false;
             }
         };
 
-        // check for loacl storage support
-        if (supports_html5_storage()) {
-            var $this = $(this),
-            store = {
-                get: function() {
-                    var a = JSON.parse(localStorage.getItem(settings.savedDataName));
-                    return (typeof(a) === 'object' && a != null) ? a : {};
-                },
-                set: function(data) {
-                    localStorage.setItem(settings.savedDataName, JSON.stringify(data));
-                },
-                destroy: function() {
-                    localStorage.removeItem(settings.savedDataName);
-                }
-            },
-            key = {
-                // this key generations stuff can and should be improved
-                create: function(element) {
-                    for (var i = 0; i < settings.idAttribs.length; i++) {
-                        // check if attribute has value
-                        if ($(element).attr(settings.idAttribs[i]) != null) {
-                            // escape strings
-                            return escape(settings.idAttribs[i]) + '~' + escape($(element).attr(settings.idAttribs[i]));
-                        }
-                    }
+        if (settings.clearOnSubmit) {
+            // Most of this can be deleted when form domains are implemented
+            /*var input = $('input[type=submit]');
+            var form = input.length > 0 ? $(input[0].form) : $();*/
 
-                    return false;
-                },
-                get: function(key) {
-                    // split
-                    var a = key.split(/~(.+)?/);
-                    // unescape
-                    for (var i = 0; i < a.length; i++) {
-                        a[i] = unescape(a[i]);
-                    }
-                    return a;
-                }
-            },
-            elementData = {
-                save: function(element) {
-                    if (element) {
-                        // get data
-                        var data = store.get();
-
-                        var elementId = key.create(element);
-                        if (elementId) {
-                            settings.beforeSave.call();
-
-                            var now = new Date().getTime();
-                            var expires = (settings.expireInMs) ? parseInt(now.toString()) + settings.expireInMs : null;
-
-                            var toSave = "";
-                            switch ($(element).attr('type')){
-                              case 'radio':
-                              case 'checkbox':
-                                toSave = $(element).prop('checked');
-                                break;
-                              case 'select':
-                              case 'input':
-                              default:
-                                toSave = $(element).val();
-                            }
-
-                            data[elementId] = {
-                                                expires: expires,
-                                                val: toSave
-                                            };
-
-                            // save data
-                            store.set(data);
-                            settings.onSave.call();
-                            return true;
-                        }
-                    }
-
-                    return false;
-                },
-                destroy: function(element) {
-                    if (element) {
-                        // get data
-                        var data = store.get();
-
-                        var elementId = key.create(element);
-                        if (elementId) {
-                            settings.beforeDelete.call();
-
-                            delete data[elementId];
-
-                            settings.onDelete.call();
-                            // save data
-                            store.set(data);
-                            return true;
-                        }
-                    }
-
-                    return false;
-                },
-                hasExpired: function(data) {
-                    // check if element has expired
-                    var now = new Date().getTime();
-                    return (data.expires) ? (now > data.expires) : false;
-                }
-            };
-
-            if (settings.clearOnSubmit) {
-                // Most of this can be deleted when form domains are implemented
-                /*var input = $('input[type=submit]');
-                var form = input.length > 0 ? $(input[0].form) : $();*/
-
-                this.closest('form')
-                    .submit(function(e){
-                        e.preventDefault();
-                        // when closest form is submitted
-                        $this.each(function(index, element){
-                            // delete element data
-                            elementData.destroy(this);
-                        });
-                });
-            }
-
-            if (settings.saveOnInterval && !isNaN(settings.saveOnInterval) && parseInt(settings.saveOnInterval) > 0) {
-                setInterval(function(){
-                    // save input
-                    elementData.save(this);
-                }, parseInt(settings.saveOnInterval));
-            }
-
-            $(document).ready(function() {
-                settings.beforeLoad.call();
-
-                var data = store.get();
-                for (var k in data) {
-                    // loop through saved data
-                    // check if data has expired
-                    if (!elementData.hasExpired(data[k])) {
-                        // get key
-                        var id = key.get(k);
-                        // find respective element & set saved data
-                        var $element = $('[' + id[0] + '="' + id[1] + '"]');
-
-                        switch ($element.attr('type')){
-                          case 'radio':
-                          case 'checkbox':
-                            $element.prop('checked', data[k].val);
-                            break;
-                          case 'select':
-                            $element.find("option").filter(function() {
-                                // may want to use $.trim in here
-                                return $(this).text() == data[k].val;
-                            }).attr('selected', true);
-                            break;
-                          default:
-                            $element.val(data[k].val);
-                        }
-                    } else {
-                        // if data expired.. delete
-                        delete data[key];
-                    }
-                }
-
-                // save data.. in case some keys have been deleted
-                store.set(data);
-
-                settings.onLoad.call();
+            this.closest('form')
+                .submit(function(e){
+                    e.preventDefault();
+                    // when closest form is submitted
+                    $this.each(function(index, element){
+                        // delete element data
+                        elementData.destroy(this);
+                    });
             });
-
-            if (settings.saveOnChange) {
-              this.change(function(){
-                  elementData.save(this);
-              });
-            }
         }
 
-        return this;
+        if (settings.saveOnInterval && !isNaN(settings.saveOnInterval) && parseInt(settings.saveOnInterval) > 0) {
+          setInterval(
+            function(that){
+              for (var i = 0; i < that.length; i++) {
+                elementData.save(that[i]);
+              }
+            },
+            parseInt(settings.saveOnInterval),
+            this
+          );
+        }
+
+        $(document).ready(function() {
+            settings.beforeLoad.call();
+
+            var data = store.get();
+            for (var k in data) {
+                // loop through saved data
+                // check if data has expired
+                if (!elementData.hasExpired(data[k])) {
+                    // get key
+                    var id = key.get(k);
+                    // find respective element & set saved data
+                    var $element = $('[' + id[0] + '="' + id[1] + '"]');
+
+                    switch ($element.attr('type')){
+                      case 'radio':
+                      case 'checkbox':
+                        $element.prop('checked', data[k].val);
+                        break;
+                      case 'select':
+                        $element.find("option").filter(function() {
+                            // may want to use $.trim in here
+                            return $(this).text() == data[k].val;
+                        }).attr('selected', true);
+                        break;
+                      default:
+                        $element.val(data[k].val);
+                    }
+                } else {
+                    // if data expired.. delete
+                    delete data[key];
+                }
+            }
+
+            // save data.. in case some keys have been deleted
+            store.set(data);
+
+            settings.onLoad.call();
+        });
+
+        if (settings.saveOnChange) {
+          this.change(function(){
+              elementData.save(this);
+          });
+        }
     }
 
-    // defaults may be overridden
-    $.fn.ftakar.defaults = {
-        savedDataName: 'FTAKAR',
-        // saveOnInterval: false,
-        // saveOnInterval: 2000,
-        saveOnInterval: false, // TODO
-        saveOnChange: true,
-        clearOnSubmit: true,
-        expireInMs: false,
-        // priority by order of attribute
-        idAttribs: ['id', 'name', 'data-ftakar'],
-        beforeSave: function(){ console.info('FTAKAR: data is being saved', $(this)) },
-        // $(document).trigger( "ftakar__beforeSave" ); // TODO
-        onSave: function(){ console.info('FTAKAR: data saved', $(this)) },
-        // $(document).trigger( "ftakar__onSave" ); // TODO
-        beforeLoad: function(){ console.info('FTAKAR: data is being loaded') },
-        // $(document).trigger( "ftakar__beforeLoad" ); // TODO
-        onLoad: function(){ console.info('FTAKAR: data has loaded') },
-        // $(document).trigger( "ftakar__onLoad" ); // TODO
-        beforeDelete: function(){ console.info('FTAKAR: element data is going to be deleted', $(this)) },
-        // $(document).trigger( "ftakar__beforeDelete" ); // TODO
-        onDelete: function(){ console.info('FTAKAR: element data deleted', $(this)) },
-        // $(document).trigger( "ftakar__onDelete" ); // TODO
-        beforeClear: function(){ console.info('FTAKAR: data is going to be cleared') },
-        // $(document).trigger( "ftakar__beforeClear" ); // TODO
-        onClear: function(){ console.info('FTAKAR: data cleared') }
-        // $(document).trigger( "ftakar__onClear" ); // TODO
-    };
+    return this;
+  }
+
+  // defaults may be overridden
+  $.fn.ftakar.defaults = {
+    savedDataName: 'FTAKAR',
+    // saveOnInterval: 2000,
+    saveOnInterval: false,
+    saveOnChange: true,
+    clearOnSubmit: true,
+    expireInMs: false,
+    // priority by order of attribute
+    idAttribs: ['id', 'name', 'data-ftakar'],
+    beforeSave: function(){ console.info('FTAKAR: data is being saved', $(this)) },
+    // $(document).trigger( "ftakar__beforeSave" ); // TODO
+    onSave: function(){ console.info('FTAKAR: data saved', $(this)) },
+    // $(document).trigger( "ftakar__onSave" ); // TODO
+    beforeLoad: function(){ console.info('FTAKAR: data is being loaded') },
+    // $(document).trigger( "ftakar__beforeLoad" ); // TODO
+    onLoad: function(){ console.info('FTAKAR: data has loaded') },
+    // $(document).trigger( "ftakar__onLoad" ); // TODO
+    beforeDelete: function(){ console.info('FTAKAR: element data is going to be deleted', $(this)) },
+    // $(document).trigger( "ftakar__beforeDelete" ); // TODO
+    onDelete: function(){ console.info('FTAKAR: element data deleted', $(this)) },
+    // $(document).trigger( "ftakar__onDelete" ); // TODO
+    beforeClear: function(){ console.info('FTAKAR: data is going to be cleared') },
+    // $(document).trigger( "ftakar__beforeClear" ); // TODO
+    onClear: function(){ console.info('FTAKAR: data cleared') }
+    // $(document).trigger( "ftakar__onClear" ); // TODO
+  };
 }));
